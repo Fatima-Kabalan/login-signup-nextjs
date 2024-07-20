85d2edf79e1a423afa594d7b7e4af913c8450463
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-07-20 15:55:10 +0300

    creating LoginForm component

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,3 +1,9 @@
+import LoginForm from "@/components/LoginForm";
+
 export default function Login() {
-  return <div>login</div>;
+  return (
+    <>
+      <LoginForm />
+    </>
+  );
 }
---
+++ components/LoginForm.tsx
@@ -0,0 +1,54 @@
+"use client";
+
+import { useState } from "react";
+
+export default function LoginForm() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    // Handle login logic here
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="max-w-md mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+    >
+      <h1 className="text-2xl font-bold flex justify-center mb-6">
+        Welcome to Login Page!
+      </h1>
+      <div className="mb-4">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          Email
+        </label>
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+      </div>
+      <div className="mb-6">
+        <label className="block text-gray-700 text-sm font-bold mb-2">
+          Password
+        </label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+        />
+      </div>
+      <div className="flex items-center justify-between">
+        <button
+          type="submit"
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+        >
+          Login
+        </button>
+      </div>
+    </form>
+  );
+}
