e006e1126346f79735788637e2d66be6cd1396d4
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-07-28 12:13:17 +0300

    adding a header to Register page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -1,3 +1,10 @@
-export default function SignUp() {
-  return <h1>signup</h1>;
+import SignupForm from "@/components/SignupForm";
+
+export default function Register() {
+  return (
+    <div className="container mx-auto">
+      <h1 className="text-2xl font-bold text-center mt-8">Register</h1>
+      <SignupForm />
+    </div>
+  );
 }
