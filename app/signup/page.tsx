664847f2c57fa04a7758ee7e7cb15e5fad511996
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-08-03 22:05:10 +0300

    adding packages, updating the page

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -2,9 +2,10 @@ import SignupForm from "@/components/SignupForm";
 
 export default function Register() {
   return (
-    <div className="container mx-auto">
-      <h1 className="text-2xl font-bold text-center mt-8">Register</h1>
-      <SignupForm />
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <div className="max-w-md w-full">
+        <SignupForm />
+      </div>
     </div>
   );
 }
