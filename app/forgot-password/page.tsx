08da119e8f686ab72c296d312cc4a64c9ac9ca30
Fatima-Kabalan <kabalan.fatima01@gmail.com>
2024-08-11 21:04:06 +0300

    creating forgot password page

---
+++ app/forgot-password/page.tsx
@@ -0,0 +1,63 @@
+"use client";
+import { useState } from "react";
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch("/api/auth/forgot-password", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      // Make sure the response is read only once
+      const data = await res.json();
+
+      if (res.ok) {
+        setMessage("Password reset link sent to your email");
+      } else {
+        setMessage(data.message || "Something went wrong");
+      }
+    } catch (error) {
+      console.error("Error during password reset:", error);
+      setMessage("An error occurred. Please try again.");
+    }
+  };
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col  w-full max-w-md border rounded-sm shadow-lg bg-white p-8 gap-4"
+      >
+        <h1 className="font-bold text-2xl text-center text-gray-800">
+          Forgot Password
+        </h1>
+        {message && <p className="mb-4 text-center text-red-500">{message}</p>}
+        <div className="flex flex-col gap-2">
+          <label className="block text-gray-700 text-sm font-bold">Email</label>
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none"
+            required
+          />
+        </div>
+        <button
+          type="submit"
+          className="w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
+        >
+          Send Reset Link
+        </button>
+      </form>
+    </div>
+  );
+}
